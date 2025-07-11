5608878c6df1e39bfc794511da5bd0866f786605
lawrence musonda <lawrencemusonda555@gmail.com>
2025-07-11 20:33:41 +0100

    improved the frontend

--- mytodo/urls.py
+++ mytodo/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("", views.home, name="mytodo_home"),
     path('mark_completed/<int:task_id>/', views.mark_completed, name='mark_completed'),
     path('delete_task/<int:task_id>/', views.delete_task, name='delete_task'),
+    path('add_task/', views.add_task, name='add_task'),
 ]
--- mytodo/views.py
+++ mytodo/views.py
@@ -21,3 +21,11 @@ def delete_task(request, task_id):
         task = get_object_or_404(Task, id=task_id)
         task.delete()
     return redirect('mytodo_home')
+
+def add_task(request):
+    if request.method == "POST":
+        title = request.POST.get('title')
+        description = request.POST.get('description')
+        if title:
+            Task.objects.create(title=title, description=description, user=request.user)
+    return redirect('mytodo_home')                                                                                                  
